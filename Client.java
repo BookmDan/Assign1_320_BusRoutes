23e0277c8922f4eb0668f42834b151918c852380
BookmDan <danoh329@gmail.com>
2023-02-11 14:08:59 -0800

    clean input for user input

--- Client.java
+++ Client.java
@@ -14,10 +14,33 @@ import java.util.regex.Pattern;
 public class Client { // implements IRouteFinder
   public static void main(String[] args) throws IOException {
     RouteFinder finder = new RouteFinder();
-    Map<String, List<String>> routeUrls = finder.getBusRoutesUrls('b');
     
-    finder.getBusRouteTripsLengthsInMinutesToAndFromDestination(routeUrls.get("Bellevue"));
+    // asks for destination intial input from user
+    System.out.println("Enter city destination initial:");
 
+    BufferedReader inp = new BufferedReader(new InputStreamReader(System.in));
+    // int T= Integer.parseInt(inp.readLine()); // for taking a number as an input 
+    Map<String, List<String>> routeUrls;
+    try {
+      
+      String str = inp.readLine();
+      char c = str.charAt(0);
+      routeUrls = finder.getBusRoutesUrls(c);
+
+      // asks for destination name input from user 
+      System.out.println("Enter city destination:");
+      
+      String str2 = inp.readLine();
+
+      // should return the calculated distances but it does not do this currently
+      finder.getBusRouteTripsLengthsInMinutesToAndFromDestination(finder.getBusRoutesUrls(c).get(str2));
+    } catch (Exception e) {
+      // TODO: handle exception
+    }
+
+     // Map<String, List<String>> routeUrls = finder.getBusRoutesUrls('b');
+     // finder.getBusRouteTripsLengthsInMinutesToAndFromDestination(routeUrls.get("Bellevue"));
+    
     // does not work 
     // finder.printLine(routeUrls.get("Bellevue"));
     
